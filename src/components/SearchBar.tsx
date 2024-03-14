bfcc26a7e6e0b802bc5ec9a70abf041ec0df68e2
José de Jesús Acevedo Flores <jjaf@MacBook-Air-de-Jose-2.local>
2024-03-13 19:38:55 -0600

    add Navbar

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,59 @@
+import { FC, useState } from "react";
+import { useAutoConnect } from "../contexts/AutoConnectProvider";
+import dynamic from "next/dynamic";
+import NetworkSwitcher from "../components/NetworkSwitcher";
+import Image from "next/image";
+import ZKore from "../../public/ZKoreIcon.svg";
+import { TruckIcon } from "@heroicons/react/outline";
+
+const WalletMultiButtonDynamic = dynamic(
+  async () =>
+    (await import("@solana/wallet-adapter-react-ui")).WalletMultiButton,
+  { ssr: false }
+);
+
+const SearchBar: FC = () => {
+  const { autoConnect, setAutoConnect } = useAutoConnect();
+  const [toggle, setToggle] = useState<boolean>(true);
+
+  return (
+    <div>
+      {/*<div className="flex gap-10 flex-col md:flex-row md:justify-evenly justify-evenly items-center mt-5">
+      <Image src={ZKore} alt="Icon"/>
+      <label className="flex flex-col items-center gap-2">
+        <span>Autoconnect</span>
+        <input
+          type="checkbox"
+          checked={autoConnect}
+          onChange={(e) => setAutoConnect(e.target.checked)}
+          className="toggle"
+        />
+      </label>
+      <NetworkSwitcher />
+      <WalletMultiButtonDynamic className="flex items-center" />
+    </div> */}
+      {toggle ? (
+        <div className=" flex justify-between mr-[25px] ml-[25px] mt-[36px]">
+          <div className=" flex flex-col gap-[8px]">
+            <span className="flex gap-[8px] items-center">
+              <Image src={ZKore} alt="icon" />
+              <h1 className="text-[21px] font-semibold">ZKore</h1>
+            </span>
+            <span className="text-[10px] text-[#777E90] font-normal">
+              <h1>Discover, Review, Earn</h1>
+            </span>
+          </div>
+          <div className="flex gap-[16px] text-[#65676B]">
+            <i className="ri-heart-fill" style={{ fontSize: "24px" }}></i>
+            <i className="ri-user-3-fill" style={{ fontSize: "24px" }}></i>
+            <i className="ri-menu-5-line" style={{ fontSize: "24px" }}></i>
+          </div>
+        </div>
+      ) : (
+        <div></div>
+      )}
+    </div>
+  );
+};
+
+export default SearchBar;
---
+++ src/views/search/search.tsx
@@ -0,0 +1,14 @@
+// src/views/home/ProductGallery.tsx
+import { FC } from "react";
+import Link from "next/link";
+
+
+const ProductGallery: FC = () => {
+  return (
+    <div className="flex flex-col gap-[40px]">
+        
+    </div>
+  );
+};
+
+export default ProductGallery;
