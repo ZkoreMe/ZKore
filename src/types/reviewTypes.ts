9bc8ce597d766b984cf999aeb33c3de43acfaa38
José de Jesús Acevedo Flores <jesusaf@Joses-MacBook-Air.local>
2024-03-20 17:28:47 -0600

    Review detail page

---
+++ src/types/reviewTypes.ts
@@ -0,0 +1,12 @@
+export interface ReviewDetails {
+    id: number;
+    user: string;
+    date: string;
+    img: string;
+    tittle: string;
+    rate: number;
+    description: string;
+    like: number;
+    dislike: number;
+    referral: string;
+  }
