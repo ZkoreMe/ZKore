58702ec27db01a8cff906b5f7962f317af2f4174
Timothy Sukamtoh <timtoh168@gmail.com>
2024-04-07 13:45:44 -0700

    modified sign_message fn

--- program/src/instructions/sign_message.rs
+++ program/src/instructions/sign_message.rs
@@ -1,21 +1,9 @@
 use anchor_lang::prelude::*;
 use solana_program::sysvar::clock::Clock;
 
-#[access_control(sign_message(&ctx))]
-pub fn sign_message(ctx: Context<SignMessage>) -> ProgramResult {
-    // Emit an instruction to prompt the user to sign a message
-    let accounts = &[ctx.accounts.signer];
-    let message = b"Confirm Referral Link Redirect";
-    let ix = anchor_lang::solana_program::instruction::Instruction {
-        program_id: ctx.program_id,
-        accounts: accounts.to_vec(),
-        data: message.to_vec(),
-    };
+pub fn sign_message_(_ctx: Context<SignMessage>) -> Result<()> {
+    msg!("Confirm Referral Link Redirect");
 
-    // Invoke a system instruction to invoke the instruction to prompt the user to sign
-    solana_program::program::invoke_signed(&ix, &[&ctx.accounts.signer], &[])?;
-
-    // Get the current timestamp
     let current_timestamp = Clock::get()?.unix_timestamp;
 
     // Emit an event to indicate that the message has been signed
