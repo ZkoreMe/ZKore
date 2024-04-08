d9c734b285bc98c1cd6b618516751476975d8a7c
Timothy Sukamtoh <timtoh168@gmail.com>
2024-04-08 15:49:53 -0700

    Revert "changed sign_message to post fn"
    This reverts commit eeb08f44b00e2245829e37be30fb16e338d162be.

--- program/src/instructions/mod.rs
+++ program/src/instructions/mod.rs
@@ -2,10 +2,10 @@ pub use buy_product::*;
 pub use create_product::*;
 pub use create_review::*;
 pub use init_accounts::*;
-pub use post::*;
+pub use sign_message::*;
 
 pub mod buy_product;
 pub mod create_product;
 pub mod create_review;
 pub mod init_accounts;
-pub mod post;
+pub mod sign_message;
